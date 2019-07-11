35b178004098885b41661ba93bfd3c8a2eb3698e
quannv-rikkei <quannv@rikkeisoft.com>
2019-07-11 10:56:06 +0700

    TRA_02.T01.03.Intent_IntentFilter

--- TRA_02T0103Intent_IntentFilter/app/src/main/java/com/rikkei/tra_02t0103intent_intentfilter/MainActivity.kt
+++ TRA_02T0103Intent_IntentFilter/app/src/main/java/com/rikkei/tra_02t0103intent_intentfilter/MainActivity.kt
@@ -1,25 +1,142 @@
 package com.rikkei.tra_02t0103intent_intentfilter
 
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.os.Parcelable
+import android.provider.MediaStore
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.MediaController
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+import java.io.File
+import java.util.jar.Manifest
+
+const val VIDEO_CAPTURE = 101
+const val REQUEST_PERMISSION = 101
 
 class MainActivity : AppCompatActivity() {
 
+    val listPermission = arrayOf(
+        android.Manifest.permission.CAMERA,
+        android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+    )
+
+
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
+        handleIntent()
+
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+            if (checkSelfPermission(listPermission[0]) != PackageManager.PERMISSION_GRANTED
+                || checkSelfPermission(listPermission[1]) != PackageManager.PERMISSION_GRANTED
+            ) {
+                requestPermissions(listPermission, REQUEST_PERMISSION)
+            } else {
+                captureVideo()
+            }
         }
     }
+
+    private fun handleIntent() {
+        if (intent != null && intent.action == Intent.ACTION_SEND) {
+            when {
+                intent.type.startsWith("image/") -> {
+                    handleSendImage(intent)
+                }
+                intent.type.startsWith("video/") -> {
+                    handleSendVideo(intent)
+                }
+                else -> {
+
+                }
+            }
+        }
+    }
+
+    private fun handleSendImage(intent: Intent) {
+        (intent.getParcelableExtra<Parcelable>(Intent.EXTRA_STREAM) as? Uri)?.let {
+            // Update UI to reflect image being shared
+            ivItem.setImageURI(intent.getParcelableExtra(Intent.EXTRA_STREAM));
+        }
+    }
+
+    private fun handleSendVideo(intent: Intent) {
+        (intent.getParcelableExtra<Parcelable>(Intent.EXTRA_STREAM) as? Uri)?.let {
+            // Update UI to reflect image being shared
+            videoView.setVideoURI(intent.getParcelableExtra(Intent.EXTRA_STREAM))
+            videoView.setMediaController(MediaController(this))
+            videoView.requestFocus()
+            videoView.start()
+        }
+    }
+
+
+    private fun captureVideo() {
+        if (packageManager.hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY)) {
+
+            /*val mediaFile = File(Environment.getExternalStorageDirectory().absolutePath + "/test_video.mp4")
+            val uriVideo = Uri.fromFile(mediaFile)*/
+
+
+            val intent = Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+            intent.putExtra(MediaStore.EXTRA_DURATION_LIMIT, 10)
+            intent.putExtra(
+                MediaStore.EXTRA_OUTPUT,
+                Environment.getExternalStorageDirectory().absolutePath + "/test_video.mp4"
+            );
+            startActivityForResult(intent, VIDEO_CAPTURE);
+
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_PERMISSION) {
+            var isGranted = true
+            if (grantResults != null && !grantResults.isEmpty()) {
+                for (item in grantResults) {
+                    if (PackageManager.PERMISSION_DENIED == item) {
+                        isGranted = false
+                        break
+                    }
+                }
+            }
+            if (isGranted) {
+                captureVideo()
+            } else {
+                Toast.makeText(this, "You must grant permission! ", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (VIDEO_CAPTURE == requestCode) {
+            if (resultCode == Activity.RESULT_OK) {
+                Toast.makeText(this, "Video saved to: " + data?.data, Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Video recording cancelled", Toast.LENGTH_SHORT).show()
+            }
+        } else {
+            Toast.makeText(this, "Failed to record video", Toast.LENGTH_SHORT).show()
+        }
+    }
+
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
